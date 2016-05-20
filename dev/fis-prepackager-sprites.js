937186e6a4a048431a585ae679ccde292da7210e
liyh <lyh106415@alibaba-inc.com>
2016-05-20 14:14:36 +0800

    Initial Commit

---
+++ dev/fis-prepackager-sprites.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function (ret, conf, settings, opt) {
+    var Sprite = require('../lib/sprites.com.js').default;
+
+    var sprite = new Sprite();
+
+    sprite.outputImage = function(options) {
+        var outputPath = options.outputPath;
+        var content = options.content;
+        var opt = options.opt;
+
+        var file = fis.file.wrap(outputPath);
+        file.setContent(content);
+        fis.compile(file);
+        ret.pkg[opt.image] = file;
+    };
+
+    sprite.outputStyle = function(options) {
+        var outputPath = options.outputPath;
+        var content = options.content;
+        var opt = options.opt;
+
+        var file = fis.file.wrap(outputPath);
+        file.setContent(content);
+        fis.compile(file);
+        console.log(file.getContent());
+        ret.pkg[opt.style] = file;
+    };
+
+    sprite.run();
+};
